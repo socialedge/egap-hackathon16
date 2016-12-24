7a42b103a0476c03b3798d8d0331a8e897909835
Elias Ovchynnikov <illia.ovchynnikov@gmail.com>
2016-12-24 22:54:05 +0200

    CRUDify Receipt Repository (#4)

--- src/main/java/com/receipts/on/ReceiptResource.java
+++ src/main/java/com/receipts/on/ReceiptResource.java
@@ -1,11 +1,20 @@
 package com.receipts.on;
 
+import com.google.gson.Gson;
+import com.receipts.on.model.Prescription;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Optional;
+
 import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class ReceiptResource {
 
     private static final String API_CONTEXT = "/api/v1";
+
+    private static final Gson GSON = new Gson();
  
     private final ReceiptRepository receiptRepository;
  
@@ -15,19 +24,30 @@ public class ReceiptResource {
     }
  
     private void setupEndpoints() {
-        post(API_CONTEXT + "/receipts", "application/json", (request, response) -> {
-            receiptRepository.createNew(request.body());
-            response.status(201);
+        post(API_CONTEXT + "/prescriptions", "application/json", (request, response) -> {
+            Prescription prescription = GSON.fromJson(request.body(), Prescription.class);
+
+            if (receiptRepository.create(prescription))
+                response.status(HttpServletResponse.SC_CREATED);
+            else
+                response.status(HttpServletResponse.SC_CONFLICT);
+
             return response;
         }, new JsonTransformer());
+
+        get(API_CONTEXT + "/prescriptions/:id", "application/json", (request, response) -> {
+            Long prescriptionId = Long.valueOf(request.params(":id"));
+            Optional<Prescription> prescriptionOpt = receiptRepository.find(prescriptionId);
+
+            if (!prescriptionOpt.isPresent()) {
+                response.status(HttpServletResponse.SC_NOT_FOUND);
+                return null;
+            }
+
+            return prescriptionOpt.get();
+        }, new JsonTransformer());
  
-        get(API_CONTEXT + "/receipts/:id", "application/json", (request, response)
- 
-                -> receiptRepository.find(request.params(":id")), new JsonTransformer());
- 
-        get(API_CONTEXT + "/receipts", "application/json", (request, response)
- 
-                -> receiptRepository.findAll(), new JsonTransformer());
- 
+        get(API_CONTEXT + "/prescriptions", "application/json",
+                (request, response) -> receiptRepository.findAll(), new JsonTransformer());
     }
 }
